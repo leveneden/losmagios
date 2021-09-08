d55ec3d83f868db8e51c6e77d9484685732cac2f
andy <arteysoft@gmail.com>
2021-09-07 22:07:49 -0300

    clase #1

--- clase1/src/main/java/edu/it/App.java
+++ clase1/src/main/java/edu/it/App.java
@@ -1,7 +1,13 @@
 package edu.it;
 
 public class App {
+    private static int leerTeclado (String mensaje){
+        java.util.Scanner teclado = new java.util.Scanner(System.in);
+        System.out.println(mensaje);
+        return teclado.nextInt();
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Curso JAVA");
+        System.out.println("");
+        System.out.println(ConjeturaCollatz.corroborar(111L));
     }
 }
